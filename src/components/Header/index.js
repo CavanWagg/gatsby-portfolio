8faae2984dbd508130fe859ba8d65bcb76165e44
Cavan Wagg <waggx002@gmail.com>
2020-01-27 08:15:00 -0600

    fix headShot

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,23 @@
 import React from "react"
 import Footer from '../Footer'
+import headShot from "../../../static/wagg-cavan.png"
+
 
 export default () => {
   return (
-  <header className="main-grid header">
-    <div className="header-content">
-      <div href="#" className="avatar"
-      ><img
-          className="face"
-          src={"/images/wagg-cavan.png"}
-          alt="Cavan Wagg - Front End Developer"
-        /></div>
-      <h1 className='header-title'>
-       I'm <span>Cavan</span> <br/><strong>front-end dev</strong>
-      </h1>
-    </div>
-    <Footer footerType="footer-landscape" />
-  </header>
-  )}
+    <header className="main-grid header">
+      <div className="header-content">
+        <div href="#" className="avatar"
+        ><img
+            className="face"
+            src={headShot}
+            alt="Cavan Wagg - Front End Developer"
+          /></div>
+        <h1 className='header-title'>
+          I'm <span>Cavan</span> <br /><strong>front-end dev</strong>
+        </h1>
+      </div>
+      <Footer footerType="footer-landscape" />
+    </header>
+  )
+}
