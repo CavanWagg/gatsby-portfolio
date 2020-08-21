634aa21f366920fab25fb8b7a7dabe23d7a7adb4
Cavan <waggx002@gmail.com>
2020-08-20 20:34:42 -0500

    retainable embed not working in prod, temp disabling

--- src/components/Main/Blog.js
+++ src/components/Main/Blog.js
@@ -7,15 +7,21 @@ export default () => {
         <header className="major">
           <h2>Blog</h2>
         </header>
-        <div
+        <a
+          target="_blank"
+          href="https://medium.com/@waggx002/differentiate-between-a-click-and-swipe-drag-event-in-react-f141e1ff3162"
+        >
+          Differentiate between a Click and Swipe/Drag event in React
+        </a>
+        {/* <div
           id="retainable-rss-embed"
           data-rss="https://medium.com/feed/@waggx002"
           data-maxcols="1 "
-          data-layout="grid"
+          data-layout="slider"
           data-poststyle="external"
           data-readmore="Read the rest"
           data-buttonclass="readmore"
-        />
+        /> */}
       </section>
     </div>
   )
